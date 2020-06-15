62a2ee15d21dc999f70756961651e44534ce8160
lukacukeric77 <lukacukeric77@gmail.com>
2020-06-15 14:54:55 +0200

    Werknemershierarchie functions completely; Next: - Onderschrikten for lowest level does not function. Why? - Jobtitel page - Opslag and Rijkregisternummer

--- src/main/java/be/vdab/personeel/controllers/WerknemerController.java
+++ src/main/java/be/vdab/personeel/controllers/WerknemerController.java
@@ -1,7 +1,6 @@
 package be.vdab.personeel.controllers;
 
 import be.vdab.personeel.domain.Werknemer;
-import be.vdab.personeel.services.JpaJobtitelServices;
 import be.vdab.personeel.services.JpaWerknemerServices;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,33 +15,34 @@ import java.util.Optional;
 public class WerknemerController {
 
     private final JpaWerknemerServices werknemerServices;
-    private final JpaJobtitelServices jobtitelServices;
 
-    public WerknemerController(JpaWerknemerServices werknemerServices, JpaJobtitelServices jobtitelServices) {
+    public WerknemerController(JpaWerknemerServices werknemerServices) {
         this.werknemerServices = werknemerServices;
-        this.jobtitelServices = jobtitelServices;
     }
 
+
+
     @GetMapping
-    public ModelAndView ceoDisplay(){
-        ModelAndView modelAndView = new ModelAndView("werknemer");
-        Optional<Werknemer> optionalWerknemer = werknemerServices.findCeo();
-        optionalWerknemer.ifPresent(werknemer -> {
-            modelAndView.addObject("werknemer", werknemer);
-            modelAndView.addObject("ondergeschikten", werknemerServices.findByChefId(werknemer.getId()));
-        });
-        return modelAndView;
+    public ModelAndView ceoDisplay() {
+        return processor(werknemerServices.findCeo());
+
     }
 
     @GetMapping("{id}")
-    public ModelAndView werknemerDisplay(@PathVariable long id){
+    public ModelAndView werknemerDisplay(@PathVariable long id) {
+        return processor(werknemerServices.findById(id));
+    }
+
+
+
+    private ModelAndView processor(Optional<Werknemer> optionalWerknemer) {
         ModelAndView modelAndView = new ModelAndView("werknemer");
-        Optional<Werknemer> optionalWerknemer = werknemerServices.findById(id);
         optionalWerknemer.ifPresent(werknemer -> {
-            modelAndView.addObject("werknemer", werknemer);
+            modelAndView.addObject(werknemer);
             modelAndView.addObject("ondergeschikten", werknemerServices.findByChefId(werknemer.getId()));
         });
         return modelAndView;
     }
+
 
 }
